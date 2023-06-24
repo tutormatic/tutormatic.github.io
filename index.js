bc20a49ee1b3d3349435519d8c7681ca529ac95a
tutormatic <137571009+tutormatic@users.noreply.github.com>
2023-06-24 16:48:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ document.getElementById("sendEmailButton").addEventListener("click", sendEmail);
 function sendEmail() {
     const language = document.getElementById('langName').value;
     const name = document.getElementById('firstName').value;
+    const plan = document.getElementById('plan').value;
     if (language !== "") {
         if (name !== "") {
-            if (language.toUpperCase() !== "HTML" && language.toUpperCase() !== "CSS" && language.toUpperCase() !== "JAVASCRIPT" && language.toUpperCase() !== "PYTHON" && language.toUpperCase() !== "NODE.JS" && language.toUpperCase() !== "SKRIPT") {
-                alert("The language: " + language + " isn't available or isn't written correctly.");
-                return false;
+            if (plan !== "") {
+                if (language.toUpperCase() !== "HTML" && language.toUpperCase() !== "CSS" && language.toUpperCase() !== "JAVASCRIPT" && language.toUpperCase() !== "PYTHON" && language.toUpperCase() !== "NODE.JS" && language.toUpperCase() !== "SKRIPT") {
+                    alert("The language: " + language + " isn't available or isn't written correctly.");
+                    return false;
+                }
+                if (plan.toUpperCase() !== "NORMAL") {
+                    alert("You plan does not exist or is not written correctly.");
+                    return false;
+                }
+                var subject = encodeURIComponent("Tutoring for " + name);
+                var body = encodeURIComponent("Hello, I'm " + name + " and I would like to have a tutoring in the programming language " + language + " and I also choosed the " + plan.toLowerCase() + " plan.\n" +  name);
+                window.location.href = "mailto:tutormatic1@gmail.com?subject=" + subject + "&body=" + body;
             }
-            var subject = encodeURIComponent("Tutoring");
-            var body = encodeURIComponent("Hello, my name is " + name + " and I'm interested in the tutoring for the programming language called " + language + ".");
-            window.location.href = "mailto:tutormatic1@gmail.com?subject=" + subject + "&body=" + body;
         } else {
             alert("Please write your name.");
             return false;
