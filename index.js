96e9ce9db7033663241e70b71944e6bb10a10fc9
tutormatic <137571009+tutormatic@users.noreply.github.com>
2023-06-25 17:19:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ function sendEmail() {
                     alert("The language: " + language + " isn't available or isn't written correctly.");
                     return false;
                 }
-                if (plan.toUpperCase() !== "NORMAL") {
+                if (plan.toUpperCase() !== "NORMAL" && plan.toUpperCase() !== "FULL WEB") {
                     alert("You plan does not exist or is not written correctly.");
                     return false;
                 }
                 var subject = encodeURIComponent("Tutoring for " + name);
                 var body = encodeURIComponent("Hello, I'm " + name + " and I would like to have a tutoring in the programming language " + language + " and I also choosed the " + plan.toLowerCase() + " plan.\n" +  name);
                 window.location.href = "mailto:tutormatic1@gmail.com?subject=" + subject + "&body=" + body;
+            } else {
+                alert("Please select a plan.");
+                return false;
             }
         } else {
             alert("Please write your name.");
@@ -26,4 +29,23 @@ function sendEmail() {
         alert("Please write a programming language.");
         return false;
     }
+}
+function openPopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'block';
+}
+
+function selectPlan(plan) {
+    if (plan === 'Normal') {
+      console.log('Normal plan selected');
+    } else if (plan === 'Web Full') {
+      console.log('Web Full plan selected');
+    }
+
+    closePopup();
+}
+
+function closePopup() {
+    var popup = document.getElementById('popup');
+    popup.style.display = 'none';
 }
